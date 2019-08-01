12abac6e56a73b4f4643ab7a0a527583f32c3f61
Gunay <gunay.aliyeva@ytu-gunay-aliyevas-MacBook-Air.local>
2019-08-01 17:27:29 +0300

    first commit

---
+++ q1.py
@@ -0,0 +1,13 @@
+special_triangles = [(3,4,5),(5,12,13),(8,15,17),(7,24,25)]
+#print(special_triangles[1][2])
+
+class Pythagorean_Detector():
+    a=1
+    flag = False
+    while flag == True:
+        print("False")
+        if a==1:
+            print("True")
+            return True
+        return False
+    
